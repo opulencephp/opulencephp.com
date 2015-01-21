c7691715b6b878a0540f4b6bf7a898170053c0e4
ramblingsofadev <dbyoung2@gmail.com>
2015-01-20 21:28:44 -0600

    require_once calls now use Paths

--- configs/paths.php
+++ configs/paths.php
@@ -14,6 +14,10 @@ use RDev\Framework\Paths;
 $pathsConfig = [
     // The directory of this project's root directory
     "root" => __DIR__ . "/..",
+    // The application directory
+    "app" => __DIR__ . "/../app",
+    // The configs directory
+    "configs" => __DIR__,
     // The path toe the vendor (Composer) directory
     "vendor" => __DIR__ . "/../vendor",
     // The path to the view directory
