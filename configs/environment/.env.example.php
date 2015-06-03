745485af587dcb9d09c52f9387bfce2538996725
ramblingsofadev <dbyoung2@gmail.com>
2015-06-03 10:39:38 -0500

    Moved session cache bridge settings to env file

--- configs/environment/.env.example.php
+++ configs/environment/.env.example.php
@@ -5,6 +5,7 @@
  * Defines the application environment config
  */
 use RDev\Applications\Environments\Environment;
+use RDev\Cache\FileBridge;
 use RDev\Sessions\Handlers\FileSessionHandler;
 
 /** @var Environment $environment */
@@ -21,10 +22,19 @@ $environment->setVariable("DB_PORT", 5432);
 
 /**
  * ----------------------------------------------------------
- * Set the session driver
+ * Set the session handler and cache bridge
  * ----------------------------------------------------------
  */
 $environment->setVariable("SESSION_HANDLER", FileSessionHandler::class);
+$environment->setVariable("SESSION_CACHE_BRIDGE", FileBridge::class);
+
+/**
+ * ----------------------------------------------------------
+ * Set Memcached connection info
+ * ----------------------------------------------------------
+ */
+$environment->setVariable("MEMCACHED_HOST", "localhost");
+$environment->setVariable("MEMCACHED_PORT", 11211);
 
 /**
  * ----------------------------------------------------------
