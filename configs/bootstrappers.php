3d8b3b9085fcafdb1e2cf12ed6c475577baf5c48
ramblingsofadev <dbyoung2@gmail.com>
2015-09-09 19:41:42 -0500

    Removed unnecessary bootstrappers

--- configs/bootstrappers.php
+++ configs/bootstrappers.php
@@ -5,13 +5,7 @@
  * Defines the list of bootstrapper classes to load
  */
 use Opulence\Framework\Bootstrappers\Cryptography\Cryptography;
-use OpulenceWebsite\Bootstrappers\Databases\SQL;
-use OpulenceWebsite\Bootstrappers\Events\Dispatcher;
-use OpulenceWebsite\Bootstrappers\ORM\UnitOfWork;
 
 return [
-    Cryptography::class,
-    Dispatcher::class,
-    SQL::class,
-    UnitOfWork::class
+    Cryptography::class
 ];
