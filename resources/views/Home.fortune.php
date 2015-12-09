fe4a4b0260384a712922ac74c558d093a2997020
opulencephp <dbyoung2@gmail.com>
2015-12-08 23:03:57 -0600

    Updated benchmarks

--- resources/views/Home.fortune.php
+++ resources/views/Home.fortune.php
@@ -53,7 +53,7 @@
                 <h4><i class="fa fa-rocket"></i> It's fast.</h4>
                 <ul>
                     <li>
-                        Opulence can handle 350 requests/second on a cheap 512MB server and 1,500 requests/second on a modest 8GB server.
+                        Opulence can handle 500 requests/second on a cheap 512MB server and 2,000 requests/second on a modest 8GB server.
                     </li>
                     <li>
                         Most of Opulence's settings are cached to reduce overhead on each request.
