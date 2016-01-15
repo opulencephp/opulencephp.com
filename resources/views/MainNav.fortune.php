5f3313abb1ca331c8351ffbe461e32e42f27d929
opulencephp <dbyoung2@gmail.com>
2016-01-15 08:30:36 -0600

    Changed nav links

--- resources/views/MainNav.fortune.php
+++ resources/views/MainNav.fortune.php
@@ -1,10 +1,14 @@
 <nav>
     <ul>
         <li><a href="{{! route('home') !}}" title="Home">{{! logo() !}}</a></li>
-        <li class="movable{{! $request->isPath("/docs(/.*)?", true) ? ' current' : "" !}}"><a href="{{! route('docs-index') !}}" title="Documentation">Docs</a></li>
+        <li class="movable{{! $request->isPath("
+        /docs(/.*)?", true) ? ' current' : "" !}}"><a href="{{! route('docs-index') !}}"
+                                                      title="Documentation">Docs</a></li>
+        <li class="movable"><a href="https://github.com/opulencephp/Opulence" target="_blank" title="Source">Source</a>
+        </li>
         <li class="movable"><a href="/api/{{ $defaultBranch }}/" target="_blank" title="API">API</a></li>
-        <li class="movable"><a href="https://github.com/opulencephp/Opulence" target="_blank" title="GitHub">GitHub</a></li>
-        <li class="movable"><a href="https://www.twitter.com/opulencephp" target="_blank" title="David's Twitter"><i class="fa fa-twitter"></i></a></li>
+        <li class="movable"><a href="https://www.twitter.com/opulencephp" target="_blank" title="David's Twitter"><i
+                    class="fa fa-twitter"></i></a></li>
         <li id="mobile-menu" class="aux"><a href="#" title="Expand menu">&equiv;</a></li>
         <% part("docVersion") %>
 
