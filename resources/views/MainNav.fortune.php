c4a919c3ff286651d7b2572a7a3a7f4df528d630
opulencephp <dbyoung2@gmail.com>
2016-01-31 21:51:47 -0600

    Changed API link so that it actually works

--- resources/views/MainNav.fortune.php
+++ resources/views/MainNav.fortune.php
@@ -6,7 +6,7 @@
                                                       title="Documentation">Docs</a></li>
         <li class="movable"><a href="https://github.com/opulencephp/Opulence" target="_blank" title="Source">Source</a>
         </li>
-        <li class="movable"><a href="/api/{{ $defaultBranch }}/" target="_blank" title="API">API</a></li>
+        <li class="movable"><a href="/api/{{ $defaultBranch }}/index.html" target="_blank" title="API">API</a></li>
         <li class="movable"><a href="https://www.twitter.com/opulencephp" target="_blank" title="David's Twitter"><i
                     class="fa fa-twitter"></i></a></li>
         <li id="mobile-menu" class="aux"><a href="#" title="Expand menu">&equiv;</a></li>
