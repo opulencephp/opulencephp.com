91224a9eb4e847a71aaba7d5f9083d1e6be06b72
opulencephp <dbyoung2@gmail.com>
2016-02-02 08:10:07 -0600

    Fixed mobile menu

--- resources/views/SidebarNav.fortune.php
+++ resources/views/SidebarNav.fortune.php
@@ -4,8 +4,8 @@
         <ul>
             <li><a href="{{! route('home') !}}" title="Home">Home</a></li>
             <li{{! $request->isPath("/docs(/.*)?", true) ? ' class="current"' : "" !}}><a href="{{! route('docs-index') !}}" title="Documentation">Docs</a></li>
+            <li><a href="https://github.com/opulencephp/Opulence" target="_blank" title="Source">Source</a></li>
             <li><a href="/api/master/" target="_blank" title="API">API</a></li>
-            <li><a href="https://github.com/opulencephp/Opulence" target="_blank" title="GitHub">GitHub</a></li>
             <li><a href="https://www.twitter.com/opulencephp" target="_blank" title="David's Twitter"><i class="fa fa-twitter"></i></a></li>
         </ul>
     </section>
