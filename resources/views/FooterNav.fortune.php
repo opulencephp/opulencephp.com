cb6ce8990b2eeeadb4b33bd3341c3fe8d59687b5
opulencephp <dbyoung2@gmail.com>
2016-10-15 21:21:54 -0500

    Removed API docs until Sami issue is resolved

--- resources/views/FooterNav.fortune.php
+++ resources/views/FooterNav.fortune.php
@@ -5,7 +5,6 @@
         {{! $request->isPath("/docs(/.*)?", true) ? ' class="current"' : "" !}}><a href="{{! route('docs-index') !}}"
                                                                                    title="Documentation">Docs</a></li>
         <li><a href="https://github.com/opulencephp/Opulence" target="_blank" title="Source">Source</a></li>
-        <li><a href="/api/{{ $defaultBranch }}/" target="_blank" title="API">API</a></li>
         <li><a href="https://www.twitter.com/opulencephp" target="_blank" title="David's Twitter"><i
                     class="fa fa-twitter"></i></a></li>
     </ul>
--- resources/views/MainNav.fortune.php
+++ resources/views/MainNav.fortune.php
@@ -6,7 +6,6 @@
                                                       title="Documentation">Docs</a></li>
         <li class="movable main-nav"><a href="https://github.com/opulencephp/Opulence" target="_blank" title="Source">Source</a>
         </li>
-        <li class="movable main-nav"><a href="/api/{{ $defaultBranch }}/index.html" target="_blank" title="API">API</a></li>
         <li class="movable main-nav"><a href="https://www.twitter.com/opulencephp" target="_blank" title="David's Twitter"><i class="fa fa-twitter"></i></a></li>
         <li id="mobile-menu" class="aux"><a href="#" title="Expand menu">&equiv;</a></li>
         <% part("docVersion") %>
