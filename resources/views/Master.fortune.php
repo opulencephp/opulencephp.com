854c74b17e9fb02d12932587360edb8069e1d7cb
opulencephp <dbyoung2@gmail.com>
2016-11-12 13:40:43 -0600

    Fixed minor HTML validation issues

--- resources/views/Master.fortune.php
+++ resources/views/Master.fortune.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta name="viewport" content="initial-scale=1" />
         {{! charset("utf-8") !}}
@@ -10,7 +10,7 @@
         <link rel="icon" type="image/png" href="/favicon-32x32.png?v=1.0" sizes="32x32">
         <link rel="icon" type="image/png" href="/favicon-16x16.png?v=1.0" sizes="16x16">
         <link rel="manifest" href="/manifest.json?v=1.0">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1.0" color="#5bbad5">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1.0">
         <meta name="theme-color" content="#ffffff">
         {{! metaKeywords($metaKeywords) !}}
         {{! metaDescription($metaDescription) !}}
